62f6fd1689e0ede8e023c9987161270d41e4d27f
handblack <soporte@miasoftware.net>
2020-10-15 03:42:29 -0500

    asset highlight

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <footer>
       <p>Copyright &copy; 2017</p>
     </footer>
+    <script src="<?php bloginfo('template_directory'); ?>/assets/highlight/highlight.pack.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
     <?php wp_footer(); ?>
   </body>
 </html>
